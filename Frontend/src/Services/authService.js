e7e0c22bb01a738c952012892776ffb700954ef7
Yash Soni <yash2165soni@gmail.com>
2025-07-04 15:12:33 +0530

    Update authService.js

--- Frontend/src/Services/authService.js
+++ Frontend/src/Services/authService.js
@@ -1,4 +1,4 @@
-const API = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const API = 'https://digisign-backend-l83e.onrender.com';
 
 export const register = async (data) => {
   const res = await fetch(`${API}/auth/register`, {
